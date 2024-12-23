b61b534078aa10e4489662bf2fa9d8de0795b083
Miguel de la Mora <miguelalejandrodelamoraarocha@gmail.com>
2024-12-22 22:48:14 -0600

    Add and remove letters from grid/Display alert message

--- script.js
+++ script.js
@@ -2316,7 +2316,7 @@ const targetWords = [
     "shave"
   ]
   
-const dictionray = [
+const dictionary = [
     "aahed",
     "aalii",
     "aargh",
@@ -15291,6 +15291,14 @@ const dictionray = [
     "shave"
   ]
 
+const WORD_LENGTH = 5
+const alertContainer = document.querySelector("[data-alert-container]")
+const guessGrid = document.querySelector("[data-guess-grid]")
+const offsetFromDate = new Date(2024, 0, 1)
+const msOffset = Date.now() - offsetFromDate
+const dayOffset = msOffset / 1000 / 60 / 60 / 24
+const targetWord = targetWords[Math.floor(dayOffset)]
+
   startInteraction()
   
   function startInteraction(){
@@ -15322,7 +15330,6 @@ const dictionray = [
   }
 
   function handleKeyPress(e){
-    console.log(e)
     if (e.key === "Enter"){
         submitGuess()
         return
@@ -15337,4 +15344,50 @@ const dictionray = [
         pressKey(e.key)
         return
     }
+  }
+
+  function pressKey(key){
+    const activeTiles = getActiveTiles()
+    if (activeTiles.length >= WORD_LENGTH) return
+    const nextTile = guessGrid.querySelector(":not([data-letter])")
+    nextTile.dataset.letter = key.toLowerCase()
+    nextTile.textContent = key
+    nextTile.dataset.state = "active"
+  }
+
+  function deleteKey(){
+    const activeTiles = getActiveTiles()
+    const lastTile = activeTiles[activeTiles.length -1]
+    if (lastTile == null) return
+    lastTile.textContent = ""
+    delete lastTile.dataset.state
+    delete lastTile.dataset.letter
+  }
+
+  function submitGuess(){
+    const activeTiles = [...getActiveTiles()]
+    if (activeTiles.length !== WORD_LENGTH) {
+        showAlert("Not enough Letters")
+        shakeTiles(activeTiles)
+        return
+    }
+  }
+
+  function getActiveTiles(){
+    return guessGrid.querySelectorAll('[data-state="active"]')
+  }
+
+  function showAlert(message, duration = 1000) {
+    const alert = document.createElement("div")
+    alert.textContent = message
+    alert.classList.add("alert")
+    alertContainer.prepend(alert)
+    if (duration == null) return
+  
+    setTimeout(() => {
+      alert.classList.add("hide")
+      alert.addEventListener("transitionend", () => {
+        alert.remove()
+      })
+    }, duration)
   }
